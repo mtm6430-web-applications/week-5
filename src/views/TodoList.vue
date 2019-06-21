3c7eb47878cadc00ac042feb621d223b80738506
Abhay Sharma <abhaysharmawebd@gmail.com>
2019-06-21 12:15:46 -0400

    Added a form and slots in the form component

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h1>Todo List</h1>
+    <TodoForm @newTodo="addTodo">
+      <h2 slot="title">Add a Todo</h2>
+      <p slot="desc">Your Todos will be saved</p>
+    </TodoForm>
     <Todo :todos="todoList" @removeTodo="appDeleteTodo" />
   </div>
 </template>
 <script>
 import Todo from "@/components/Todo.vue";
+import TodoForm from "@/components/TodoForm.vue";
 
 export default {
   data() {
@@ -14,12 +19,28 @@ export default {
     };
   },
   components: {
-    Todo
+    Todo,
+    TodoForm
   },
   methods: {
     appDeleteTodo(index) {
       this.todoList.splice(index, 1);
+    },
+    addTodo(todo) {
+      this.todoList.push(todo);
     }
   }
 };
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  width: 50%;
+  margin: 0 auto;
+}
+ul li {
+  border-bottom: 1px solid #acacac;
+  padding: 10px 0;
+  margin-bottom: 10px;
+}
+</style>
