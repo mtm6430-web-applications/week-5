3f0e8a5b63c101df917c09c648019f8eceb14c9f
Abhay Sharma <abhaysharmawebd@gmail.com>
2019-06-21 11:17:15 -0400

    Initial Commit - Delete todo is working with a route for todo

---
+++ src/views/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <Todo :todos="todoList" @removeTodo="appDeleteTodo" />
+  </div>
+</template>
+<script>
+import Todo from "@/components/Todo.vue";
+
+export default {
+  data() {
+    return {
+      todoList: ["Walk the dogs", "Go for a ride"]
+    };
+  },
+  components: {
+    Todo
+  },
+  methods: {
+    appDeleteTodo(index) {
+      this.todoList.splice(index, 1);
+    }
+  }
+};
+</script>
